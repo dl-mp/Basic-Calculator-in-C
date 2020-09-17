af1ffeb010beca9f5c7a3ddea09eaffb8a7f0874
dn <69494267+dl-mp@users.noreply.github.com>
2020-09-18 00:42:25 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+
+
+   /*int xA, xB;
+   int Sum;
+
+   printf("Enter operation: \n");
+
+
+
+   printf("Enter two numbers: \n");
+   scanf("%d %d", &xA, &xB);
+
+   switch(calcu)
+   case '+':
+
+       void(int add)
+       break;
+
+   case '-':
+
+      break;
+
+   case '*':
+
+      break;
+
+   case '/':
+
+    break;
+
+
+
+    return 0;
+}
+
+void(int add)
+{
+    int xSum;
+    xSum = xA + xB;
+    return add;
+}
+void(int sub)
+{
+    int xDiff;
+    xDiff = xA + xB
+}
+*/
+int addT(int a, int b)
+int sub(int a, int b)
+int div(int a, int b)
+int mult(int a, int b)
+int main()
+{
+
+int a, b, result;
+printf("enter two numbers \n ");
+scanf("%d %d", &a, &b);
+printf("Select operation:\n + > addtion - > subtraction * - multiplication / > division");
+
+switch
+
+{
+    case '+':
+    int addT(int a, int b)
+    break;
+    case '-':
+
+    break;
+    case '*':
+
+    break;
+    case '/':
+
+    break;
+    default:
+    printf("failed");
+
+}
+int addT(int a, int b)
+{
+    result = a + b;
+    return addT;
+}
+int sub(int a, int b)
+{
+    result = a - b;
+    return sub;
+}
+int div(int a, int b)
+{
+    result = a / b;
+    return div;
+}
+int mult(int a, int b)
+{
+    result = a * b;
+    return mult;
+}
+
+
+
+return 0;
+}
+
+
+
+
